22a2ec51b68c84e77055fc01045b9a21669d1e13
pranav-acn <132905160+pranav-acn@users.noreply.github.com>
2025-09-03 14:45:51 +0200

    new v7

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Accenture-Agentic Engineering Orchestration-1.0.0";
 const contentToCache = [
-    "Build/Web Build v.4.loader.js",
-    "Build/Web Build v.4.framework.js",
-    "Build/Web Build v.4.data",
-    "Build/Web Build v.4.wasm",
+    "Build/Web Build v.7.loader.js",
+    "Build/Web Build v.7.framework.js",
+    "Build/Web Build v.7.data",
+    "Build/Web Build v.7.wasm",
     "TemplateData/style.css"
 
 ];
